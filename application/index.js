53a98a441d4438e0020a3ae61a3ef93b66cb7993
Edson Macoto Junior <edson.junior@gazin.com.br>
2022-02-17 11:57:24 -0300

    proxyy + balance

--- application/index.js
+++ application/index.js
@@ -1,7 +1,8 @@
 var http = require('http');
+const { networkInterfaces } = require('os');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(`<h1>${process.env.MESSAGE}</h1>`);
+  res.end(`<h1>${networkInterfaces().eth0[0].address}</h1>`);
 }).listen(8080);
